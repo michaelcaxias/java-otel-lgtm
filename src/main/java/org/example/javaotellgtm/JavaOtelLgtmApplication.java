e21570dc47b0f5344af5c5a3f3b331fe66ae742a
Michael Caxias <michael.caxias@mercadolivre.com>
2025-12-07 00:05:50 -0300

    Initialize Java project with Spring Boot, Gradle, and Docker Compose setup. Added essential configuration files, including .gitignore, .gitattributes, and Gradle wrapper. Implemented main application class and test class structure.

---
+++ src/main/java/org/example/javaotellgtm/JavaOtelLgtmApplication.java
@@ -0,0 +1,13 @@
+package org.example.javaotellgtm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOtelLgtmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaOtelLgtmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/javaotellgtm/JavaOtelLgtmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javaotellgtm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaOtelLgtmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
